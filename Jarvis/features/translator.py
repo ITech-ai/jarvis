2a69cfcd66dc28f6bdc43eb0f73dedf8dbe5666a
Arlo <omidiarad81@gmail.com>
2026-06-18 17:29:27 +0330

    Delete Jarvis directory

--- Jarvis/features/translator.py
+++
@@ -1,8 +0,0 @@
-from deep_translator import GoogleTranslator
-def transletor(text):
-    translated = GoogleTranslator(
-        source='fa',
-        target='en'
-    ).translate(text)
-
-    return translated
